9838fffac3bbdd05891c2a5bdd5b14085163c3b4
David T. Sadler <davidtsadler@googlemail.com>
2020-08-20 17:02:47 +0100

    Create custom color scheme

--- config.h
+++ config.h
@@ -7,15 +7,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_white[]       = "#ffffff";
+static const char col_gray800[]     = "#2d3748";
+static const char col_gray900[]     = "#1a202c";
+static const char col_green300[]    = "#9ae6b4";
+static const char col_green600[]    = "#38a169";
+static const char col_green900[]    = "#22543d";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg            bg            border   */
+	[SchemeNorm] = { col_green600, col_gray900,  col_green900 },
+	[SchemeSel]  = { col_white,    col_green900, col_green300 },
 };
 
 /* tagging */
@@ -56,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray900, "-nf", col_green600, "-sb", col_green900, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
